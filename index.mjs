23de758858870496c7421b5cf43c847d0d48dcf2
RazSoft <razgroupgame@gmail.com>
2026-05-06 08:38:35 +0530

    Completed the project setup
    Just completed the project setup with adding env, dotenv module and basic setup with node watch

--- index.mjs
+++ index.mjs
@@ -1,11 +1,14 @@
 import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = http.createServer((req, res) => {
-    console.log("Request is here: ", req);
+    // console.log("Request is here: ", req);
     res.statusCode = 200;
     return res.end("Here is a response");
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log("server is started");
+    console.log(`server is started at host: ${process.env.HOST} port: ${process.env.PORT}`);
 });
