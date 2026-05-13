ea5ed33a92085188c40b8f6fdd2eff518176429a
RazSoft <razgroupgame@gmail.com>
2026-05-13 23:43:37 +0530

    Enhance database connection logging and improve server response structure.
    Add server ping API end point

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -2,14 +2,18 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        console.log("DATABASE: connecting");
         await mongoose.connect(process.env.DATABASE_URI);
+        console.log("DATABSE: connected");
     } catch (err) {
         console.error("ERROR: database connection fail. ", err);
     }
 }
 const disconnectDB = async () => {
     try {
+        console.log("DATABASE: disconnecting");
         await mongoose.disconnect();
+        console.log("DATABSE: disconnected");
     } catch (err) {
         console.error("ERROR: Database can't connect ", err);
     }
--- src/index.mjs
+++ src/index.mjs
@@ -6,6 +6,17 @@ import { connectDB, disconnectDB } from "./config/db.mjs";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
+// Basic routes
+app.get("/ping", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        message: "server is running"
+    })
+});
+
+app.get("/",)
 
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || "8000";
