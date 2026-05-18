5c5a0f6ff5409235b97736cb99cbd46c75ab021d
RazSoft <razgroupgame@gmail.com>
2026-05-18 08:35:26 +0530

    Implement user registration logic and enhance task model with user reference

--- src/controllers/auth.controller.mjs
+++ src/controllers/auth.controller.mjs
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import { expressjwt } from "express-jwt";
 import bcrypt from "bcrypt";
 import { validateEmail, validateName, validatePassword } from "../utils/validations.mjs";
+import User from "./../models/User.mjs";
 
 // TODO: controller to authenticate user and send a JSON web token as response
 async function loginController(req, res) {
@@ -18,6 +19,35 @@ async function registerController(req, res) {
         const correctEmail = validateEmail(email);
         const validatePassword = validatePassword(pass);
 
+        const user = await User.findOne({ email: correctEmail });
+        if (user) {
+            return res.status(200).json({
+                status: "failed",
+                messages: "User with this email already exist",
+                data: {}
+            })
+        }
+
+        // If user don't exist then hash the password and store the user data.
+        const hashPass = await bcrypt.hash(validatePassword, 12);
+
+        const newUser = User.create({
+            name: correctName,
+            email: correctEmail,
+            password: hashPass,
+
+        })
+
+        await newUser.save();
+
+        return res.status(201).json({
+            status: "success",
+            messages: "User registration successful",
+            data: {
+                _id, name, email
+            } = newUser
+        });
+        
     } catch (err) {
         console.log("ERROR: registring user \n", err);
         return res.status(500).json({
--- src/models/Tasks.mjs
+++ src/models/Tasks.mjs
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import User from "./User.mjs";
 
-const taskSchema = mongoose.Schema({
+const taskSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true
@@ -8,6 +9,11 @@ const taskSchema = mongoose.Schema({
 
     body: {
         type: String
+    },
+
+    user: {
+        type: User,
+        required: true
     }
 }, {
     timestamp: true
