50c34ed2b7e032456679b4da03111fc22dd34f15
RazSoft <razgroupgame@gmail.com>
2026-05-18 07:58:53 +0530

    Implement user validation functions and refactor user controller

--- src/controllers/auth.controller.mjs
+++ src/controllers/auth.controller.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { expressjwt } from "express-jwt";
 import bcrypt from "bcrypt";
+import { validateEmail, validateName, validatePassword } from "../utils/validations.mjs";
 
 // TODO: controller to authenticate user and send a JSON web token as response
 async function loginController(req, res) {
@@ -10,7 +11,21 @@ async function loginController(req, res) {
 
 // TODO: controller to register a user and send the JWT token as response
 async function registerController(req, res) {
+    try {
+        const { email, name, password } = res.body;
 
+        const correctName = validateName(name);
+        const correctEmail = validateEmail(email);
+        const validatePassword = validatePassword(pass);
+
+    } catch (err) {
+        console.log("ERROR: registring user \n", err);
+        return res.status(500).json({
+            status: "failed",
+            messages: "Internal server error",
+            data: {}
+        })
+    }
 }
 
 // TODO: controller for generating new refresh token evry time the JWT expires
---
+++ src/utils/validations.mjs
@@ -0,0 +1,45 @@
+
+// Write a function to validate emails
+function validateEmail(email) {
+    if (!email)
+        return false;
+
+    email = email.trim().toLowercase();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email) ? email : false;
+}
+
+// Write a function to validate passwords
+function validatePassword(pass) {
+    if (!pass) return false;
+
+    if (pass.length < 8 || pass.length > 32) return false;
+
+    if (!/[A-Z]/.test(pass)) return false;
+
+    if (!/[a-z]/.test(pass)) return false;
+
+    if (!/\d/.test(pass)) return false;
+
+    if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(pass)) return false;
+
+    return true;
+}
+
+// Write a function to validate names
+function validateName(name) {
+    if (!name) return false;
+
+    name = name.trim().toLowercase()
+    // Only allow alphabets and spaces
+    const nameRegex = /^[a-zA-Z\s]+$/;
+    return nameRegex.test(name) ? name : false;
+}
+
+
+export {
+    validateEmail,
+    validateName,
+    validatePassword
+}
