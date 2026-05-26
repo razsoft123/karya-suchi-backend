a66f1a23cfbab50e27e243bcb01d9c6a270805fc
RazSoft <razgroupgame@gmail.com>
2026-05-26 14:05:30 +0530

    FEAT (AUTH): Add tokens utility functions.
    1. Add generate access token untility function
    2. Add generate refresh token utility function
    
    3. Add set new tokens utility function to set new token to response object

---
+++ src/utils/tokens.mjs
@@ -0,0 +1,48 @@
+// Utility function to generate the new JWT Token and Refresh Token
+import jwt from "jsonwebtoken";
+
+// add system to generate new access token
+function generateAccessToken(payload) {
+
+    const jwtToken = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TIMEOUT })
+    return jwtToken;
+
+}
+
+// add system to genreate new refresh token
+function generateRefreshToken(payload) {
+
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: process.env.JWT_REFRESH_TIMEOUT })
+    return refreshToken;
+
+}
+
+// add system to new token in response object http only cookies
+function setNewTokens(payload, res) {
+    const jwtToken = generateAccessToken(payload);
+    const refreshToken = generateRefreshToken(payload);
+
+    const baseCookieOptions = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production' ? true : false,
+        sameSite: 'strict'
+    }
+
+    res.cookie("accessToken", jwtToken, {
+        ...baseCookieOptions,
+        maxAge: process.env.ACCESS_TOKEN_MAX_AGE
+    })
+
+    res.cookie("refreshToken", refreshToken, {
+        ...baseCookieOptions,
+        maxAge: process.env.REFRESH_TOKEN_MAX_AGE
+    })
+
+    return { jwtToken, refreshToken }
+}
+
+export {
+    generateAccessToken,
+    generateRefreshToken,
+    setNewTokens
+}
